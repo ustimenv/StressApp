03ee5d0ade725e729e3a32a1d7378cc783c3fa73
ustimenv <ustimenkovlad@gmail.com>
2019-03-29 08:39:57 -0400

    Really basic but working

---
+++ app/src/main/java/com/sweng28/stressapp/ReceiverService.java
@@ -0,0 +1,68 @@
+package com.sweng28.stressapp;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.ResultReceiver;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
+public class ReceiverService extends IntentService
+{
+	public ReceiverService() {
+		super("ReceiverService");
+	}
+
+	@Override
+	protected void onHandleIntent(Intent intent)
+	{
+		Socket s = null;
+		String message = "$";
+		ServerSocket serverSocket = null;
+		InputStream is = null;
+		ResultReceiver receiver=null;
+		if(intent != null)
+		{
+			try
+			{
+				serverSocket = new ServerSocket(8080);
+				s = serverSocket.accept();
+				message = inputStreamToString(s.getInputStream());        //extract text message from received stream
+				receiver = intent.getParcelableExtra("receiver");  //send back the data to the correct activity
+				Bundle bundle = new Bundle();
+				bundle.putString("message", message);
+				receiver.send(0, bundle);
+			} catch (Exception e)
+			{
+				Log.i("1", e.getMessage());
+			}
+			finally
+			{
+				try
+				{
+					s.close();
+					serverSocket.close();
+					is.close();
+				}catch(Exception e){Log.i("1", e.getMessage());}
+			}
+		}
+	}
+	public String inputStreamToString(InputStream is) throws Exception
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(is));
+		StringBuilder sb = new StringBuilder();
+		String tmp ="";
+		while((tmp=br.readLine()) != null)
+		{
+			sb.append(tmp);
+		}
+		return sb.toString();
+	}
+	
+}
---
+++ app/src/main/java/com/sweng28/stressapp/SendMessageAsync.java
@@ -0,0 +1,38 @@
+package com.sweng28.stressapp;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class SendMessageAsync extends AsyncTask<String, Void, Void>
+{
+	@Override
+	protected Void doInBackground(String... args)
+	{
+//		final String SERVER_IP = "192.168.0.192";
+//		final String SERVER_IP = "10.1.19.47";
+		final String SERVER_IP = "192.168.43.16";
+//		final String SERVER_IP = "169.254.9.190";
+		final int SERVER_PORT = 50000;
+		
+		try(Socket socket = new Socket(InetAddress.getByName(SERVER_IP), SERVER_PORT);
+			PrintWriter writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()))))
+		{
+			StringBuilder message = new StringBuilder();
+			for(String s : args)
+			{
+				message.append(s).append('#');
+			}
+			Log.i("1", "Sends from " + socket.getLocalPort() + " to " + socket.getPort());
+			writer.write(message.toString());
+			writer.flush();
+		}catch(Exception e){Log.e("In async", e.getMessage());}
+		return null;
+	}
+}
+
