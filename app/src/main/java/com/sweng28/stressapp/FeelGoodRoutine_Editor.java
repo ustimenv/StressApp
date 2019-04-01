7592547bc4b09b78acc7d72958a92d041e6e3b03
sastaffo <sastaffo@tcd.ie>
2019-04-01 14:31:56 +0100

    moving editing to 'editing' screen (not working)

--- app/src/main/java/com/sweng28/stressapp/FeelGoodRoutine_Editor.java
+++ app/src/main/java/com/sweng28/stressapp/FeelGoodRoutine_Editor.java
@@ -1,9 +1,19 @@
 package com.sweng28.stressapp;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+import android.text.InputType;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -12,39 +22,16 @@ import android.widget.LinearLayout;
 
 import java.util.ArrayList;
 
-public class FeelGoodRoutine_Editor
+public class FeelGoodRoutine_Editor extends Activity implements NavigationView.OnNavigationItemSelectedListener
 {
-    private class FeelGoodRoutine_Editor_Entry
-    {
-        String activity;
-        CheckBox checkBox;
-        boolean isChecked;
-        static final int PADDING_CONSTANT = 50;
-        private FeelGoodRoutine_Editor_Entry(int padding)
-        {
-            int paddingTotal = padding*PADDING_CONSTANT;
-            checkBox.setPaddingRelative(checkBox.getCompoundPaddingStart(), checkBox.getCompoundPaddingTop(),
-                    paddingTotal, paddingTotal);
-		    /*checkBox.setOnClickListener(new View.OnClickListener() {
-			    @Override
-		        public void onClick(View view)
-			    {
-				    Snackbar.make(view, "Checkbox " + checkBox.getText() + " checked!", Snackbar.LENGTH_LONG)
-					    .setAction("Action", null).show();
-		        }
-    	    }); */
-        }
-        private void moveEntry(int padding)
-        {
-            int paddingTotal = padding*PADDING_CONSTANT;
-            checkBox.setPaddingRelative(checkBox.getCompoundPaddingStart(), checkBox.getCompoundPaddingTop(),
-                    paddingTotal, paddingTotal);
-        }
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        return false;
     }
 
     public LinearLayout linearLayout;
     public FeelGoodRoutine_BackEnd feelGoodRoutine;
-    public ArrayList<FeelGoodRoutine_Editor_Entry> feelGoodList_Checked;
+    // public ArrayList<FeelGoodRoutine_Editor_Entry> feelGoodList_Checked;
     Context context;
 
     public FeelGoodRoutine_Editor(FeelGoodRoutine_BackEnd fgr, LinearLayout ll, Context c)
@@ -52,6 +39,66 @@ public class FeelGoodRoutine_Editor
         this.feelGoodRoutine = fgr;
         this.linearLayout = ll;
         this.context = c;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        System.out.println("feelGoodRoutine_Editor onCreate");
+        setContentView(R.layout.feelgoodroutine_edit_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        linearLayout = findViewById(R.id.linearLayout);
+        FloatingActionButton faButton =  findViewById(R.id.fab);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+
+        System.out.println("feelGoodRoutine_Editor drawList");
+        this.drawFeelGoodList();
+
+        //setSupportActionBar(toolbar);
+        //final EditText result = (EditText) findViewById(R.id.editTextResult);
+
+        faButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                System.out.println("feelGoodRoutine_Editor faButton onClick");
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                        context);
+
+                final EditText userInput = new EditText(context);
+                userInput.setInputType(InputType.TYPE_CLASS_TEXT);
+                alertDialogBuilder.setView(userInput);
+                alertDialogBuilder.setTitle("Add New Activity");
+                // set dialog message
+                alertDialogBuilder.setPositiveButton("OK",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog,int id) {
+                                String activity = userInput.getText().toString();
+                                addFeelGoodEntry(activity);
+                            }
+                        });
+                alertDialogBuilder.setNegativeButton("Cancel",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        });
+                alertDialogBuilder.show();
+            }
+        });
+
+        drawer.bringToFront();
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+
+        navigationView.bringToFront();
+
     }
 
     public void addFeelGoodEntry(String activity)
@@ -78,9 +125,9 @@ public class FeelGoodRoutine_Editor
         entry.setText(activity);
         linearLayout.addView(entry);
     }
+
+
 /*
-
-
     void removeFeelGoodRoutineEntry(int index)
     {
 			for (int i=index; i<this.feelGoodList.size(); i++)
@@ -90,4 +137,6 @@ public class FeelGoodRoutine_Editor
             }
     }
 */
+
+
 }
