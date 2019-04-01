76ddf0cca4fd69672b9b2d75735b5bea69f75d74
sastaffo <sastaffo@tcd.ie>
2019-04-01 21:47:05 +0100

    editor working on homescreen

--- app/src/main/java/com/sweng28/stressapp/HomeScreen.java
+++ app/src/main/java/com/sweng28/stressapp/HomeScreen.java
@@ -41,7 +41,7 @@ public class HomeScreen extends AppCompatActivity implements NavigationView.OnNa
 		faButton =  findViewById(R.id.fab);
 		drawer = findViewById(R.id.drawer_layout);
 		navigationView = (NavigationView) findViewById(R.id.nav_view);
-		
+		 
 		this.feelGoodRoutineBackEnd = new FeelGoodRoutine_BackEnd();
 		this.feelGoodRoutineEditor = new FeelGoodRoutine_Editor(this.feelGoodRoutineBackEnd, this.linearLayout, getBaseContext());
 		this.feelGoodRoutineDisplay = new FeelGoodRoutine_Display(this.feelGoodRoutineEditor);
