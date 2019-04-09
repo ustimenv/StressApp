71caeafea6e06ea9e508d49f70550af7d5fdb67d
Jarota <32527884+jarota@users.noreply.github.com>
2019-04-09 16:33:07 +0100

    fixed goals adding and deleting

--- app/src/main/java/com/sweng28/stressapp/Goals.java
+++ app/src/main/java/com/sweng28/stressapp/Goals.java
@@ -19,15 +19,17 @@ import java.util.ArrayList;
 public class Goals extends ScreenTemplate
 {
     private static final String TAG = "GoalActivity";
-    private ListView mTaskListView;
+    private ListView mGoalListView;
     private ArrayAdapter<String> mAdapter;
+    public ArrayList<String> goalList;
     private FloatingActionButton addGoal;
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_goals);
-        mTaskListView = findViewById(R.id.list_goals);
+        mGoalListView = findViewById(R.id.list_goals);
+        goalList = new ArrayList<>();
         addGoal = findViewById(R.id.add_goal_fab);
         toolbar = findViewById(R.id.toolbar_goals);
         setSupportActionBar(toolbar);
@@ -36,11 +38,9 @@ public class Goals extends ScreenTemplate
         navigationView = findViewById(R.id.nav_view_goals);
         
         linearLayout = findViewById(R.id.linearLayout_goals);
-        
-    
+
         setSupportActionBar(toolbar);
-    
-    
+
     
         addGoal.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -64,6 +64,9 @@ public class Goals extends ScreenTemplate
                                 /*
                                 TODO CODE TO ADD TASK TO DB
                                  */
+                                goalList.add(goalText);
+                                mAdapter.notifyDataSetChanged();
+                                //updateUI();
                             }
                         });
                 alertDialogBuilder.setNegativeButton("Cancel",
@@ -92,49 +95,23 @@ public class Goals extends ScreenTemplate
         TextView goalTextView = parent.findViewById(R.id.goal_title);
         String goalText = String.valueOf(goalTextView.getText());
         Toast.makeText(Goals.this, "Remove Goal: " + goalText, Toast.LENGTH_LONG).show();
-        /*
-        SQLiteDatabase db = mHelper.getWritableDatabase();
-        db.delete(TaskContract.TaskEntry.TABLE,
-                TaskContract.TaskEntry.COL_TASK_TITLE + " = ?",
-                new String[]{task});
-        db.close();
-        */
-        updateUI();
+        goalList.remove(goalText);
+        mAdapter.notifyDataSetChanged();
     }
     
     
     private void updateUI() {
         
-        ArrayList<String> taskList = new ArrayList<>();
-        taskList.add("FOO");
-        taskList.add("BAR");
-        taskList.add("BINGO");
-        taskList.add("CRAP");
-        /*
-        SQLiteDatabase db = mHelper.getReadableDatabase();
-
-        Cursor cursor = db.query(TaskContract.TaskEntry.TABLE,
-                new String[]{TaskContract.TaskEntry._ID, TaskContract.TaskEntry.COL_TASK_TITLE},
-                null, null, null, null, null);
-        while (cursor.moveToNext()) {
-            int idx = cursor.getColumnIndex(TaskContract.TaskEntry.COL_TASK_TITLE);
-            taskList.add(cursor.getString(idx));
-        }
-        */
-        
         if (mAdapter == null) {
             mAdapter = new ArrayAdapter<>(this,
                     R.layout.item_goal,
                     R.id.goal_title,
-                    taskList);
-            mTaskListView.setAdapter(mAdapter);
+                    goalList);
+            mGoalListView.setAdapter(mAdapter);
         } else {
             mAdapter.clear();
-            mAdapter.addAll(taskList);
+            mAdapter.addAll(goalList);
             mAdapter.notifyDataSetChanged();
         }
-        
-        //cursor.close();
-        //db.close();
     }
 }
